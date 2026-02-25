2b3ab8fa02130ff2ef97e8307058f240380f6a3f
Fred N. van Kempen <decwiz@yahoo.com>
2026-02-25 01:15:52 -0500

    More cleanups.

--- arduino-library-at24cxxx.ino
+++ arduino-library-at24cxxx.ino
@@ -5,113 +5,139 @@
  * Because of the Arduino IDEs limitations in including and placing files
  * this file has to be placed here.
  */
-
 #include <AUnit.h>
 #include "src/at24c256.h" // trigger compilation of the source directory
+
+
+#define AT24C256_LENGTH ((uint16_t)32768)
+
 
 AT24C256 eeprom(AT24C_ADDRESS_0);
 AT24C256 badEeprom(AT24C_ADDRESS_7);
 
-#define NO_ERROR (0)
-#define ADDRESS_NACK (2)
-#define AT24C256_LENGTH ((uint16_t)32768)
-
 int memoryPosition = 0;
 int8_t buffer[1024];
 
+
 // Spread out the test values a bit in memory to spread out ware
-int getNextMemoryPosition() {
+int
+getNextMemoryPosition()
+{
   memoryPosition += 2;
   return memoryPosition;
 }
 
-test(writeAndReadByte) {
+
+test(writeAndReadByte)
+{
   int pos = getNextMemoryPosition();
   uint8_t in = 19;
+
   eeprom.write(pos, in);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   uint8_t out = eeprom.read(pos);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   assertEqual(in, out);
 }
 
-test(updateAndReadByte) {
+
+test(updateAndReadByte)
+{
   int pos = getNextMemoryPosition();
   uint8_t in = 42;
+
   eeprom.update(pos, in);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   uint8_t out = eeprom.read(pos);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   assertEqual(in, out);
 }
 
-test(putIntAndGetBack) {
+
+test(putIntAndGetBack)
+{
   int pos = getNextMemoryPosition();
   int in = 17;
+
   eeprom.put(pos, in);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   int out;
   eeprom.get(pos, out);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   assertEqual(in, out);
 }
 
-test(putDoubleAndGetBack) {
+
+test(putDoubleAndGetBack)
+{
   int pos = getNextMemoryPosition();
   double pi = 3.141593;
+
   eeprom.put(pos, pi);
-  assertEqual(eprom.getLastError(), NO_ERROR);
+  assertEqual(eprom.getLastError(), ERR_OK);
   double pi_in;
   eeprom.get(pos, pi_in);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
   assertEqual(pi_in, pi);
 }
 
-test(getLength) {
+
+test(getLength)
+{
   assertEqual(eeprom.length(), AT24C256_LENGTH);
 }
 
-test(badAddressErrorBusWorksAfter) {
+
+test(badAddressErrorBusWorksAfter)
+{
   int pos = getNextMemoryPosition();
   uint8_t in = 19;
-  assertEqual(badEeprom.getLastError(), NO_ERROR);
+
+  assertEqual(badEeprom.getLastError(), ERR_OK);
   badEeprom.write(pos, in);
-  assertEqual(badEeprom.getLastError(), ADDRESS_NACK);
+  assertEqual(badEeprom.getLastError(), ERR_NACK);
   // Verify bus is still ok after error
   eeprom.write(pos, in);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 }
 
 
-test(writeAndReadSmallBuffer) {
+test(writeAndReadSmallBuffer)
+{
   int8_t out[15] = "Test of buffer";
   int lenw = eeprom.writeBuffer(0, out, 15);
+
   assertEqual(lenw, 15);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   uint8_t in[15];
   int lenr = eeprom.readBuffer(0, in, 15);
   assertEqual(lenr, 15);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   assertEqual((char*)in, (char*)out);
 }
 
-test(writeBufferAtBadAddress) {
+
+test(writeBufferAtBadAddress)
+{
   int8_t out[15] = "Test of buffer";
   int lenw = badEeprom.writeBuffer(0, out, 15);
+
   assertEqual(lenw, 0);
-  assertEqual(badEeprom.getLastError(), ADDRESS_NACK);
+  assertEqual(badEeprom.getLastError(), ERR_NACK);
 }
 
-test(writeAndRead256Buffer) {
+
+test(writeAndRead256Buffer)
+{
   for (int i = 0; i < 255; i++) {
     buffer[i] = 'a' + (i % 26);
   }
   buffer[255] = 0;
   int lenw = eeprom.writeBuffer(0, buffer, 256);
   assertEqual(lenw, 256);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   for (int i = 0; i < 255; i++) {
     buffer[i] = '0';
@@ -119,21 +145,23 @@ test(writeAndRead256Buffer) {
 
   int lenr = eeprom.readBuffer(0, buffer, 256);
   assertEqual(lenr, 256);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   for (int i = 0; i < 255; i++) {
     assertEqual(buffer[i], (uint8_t)('a' + (i % 26)));
   }
 }
 
-test(writeAndRead1024Buffer) {
+
+test(writeAndRead1024Buffer)
+{
   for (int i = 0; i < 1023; i++) {
     buffer[i] = 'a' + (i % 26);
   }
   buffer[1023] = 0;
   int lenw = eeprom.writeBuffer(0, buffer, 1024);
   assertEqual(lenw, 1024);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   for (int i = 0; i < 1023; i++) {
     buffer[i] = '0';
@@ -141,21 +169,25 @@ test(writeAndRead1024Buffer) {
 
   int lenr = eeprom.readBuffer(0, buffer, 1024);
   assertEqual(lenr, 1024);
-  assertEqual(eeprom.getLastError(), NO_ERROR);
+  assertEqual(eeprom.getLastError(), ERR_OK);
 
   for (int i = 0; i < 1023; i++) {
     assertEqual(buffer[i], (uint8_t)('a' + (i % 26)));
   }
 }
 
-void setup() {
+
+void
+setup()
+{
   Serial.begin(115200);
   Wire.begin();
   Serial.println(F("\n\nModule test for eeprom driver\n"));
 }
 
-void loop() {
+
+void
+loop()
+{
   aunit::TestRunner::run();
 }
-
-
