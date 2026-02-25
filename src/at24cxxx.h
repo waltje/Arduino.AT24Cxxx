d9f21a2b48f2ac6ff897843f48b3dd10cb2c09e3
Fred N. van Kempen <decwiz@yahoo.com>
2026-02-25 01:09:17 -0500

    More consistency cleanups. Added error enum.

--- src/at24cxxx.h
+++ src/at24cxxx.h
@@ -16,46 +16,59 @@
 
 class AT24Cxxx {
   public:
-    AT24Cxxx(uint8_t address, TwoWire& i2c, int writeDelay, uint16_t size, uint8_t pageSize);
-    uint8_t read( int idx );
-    void write( int idx, uint8_t val);
-    void update( int idx, uint8_t val);
-    uint16_t length();
-    int writeBuffer(uint16_t address, const uint8_t* data, size_t len);
-    int readBuffer(uint16_t address, uint8_t* data, size_t len);
+    /**
+     * The meaning of the error values are:
+     *  0 .. success
+     *  1 .. length too long for buffer
+     *  2 .. address sent, NACK received - typically means no device at the address
+     *  3 .. data sent, NACK received
+     *  4 .. other twi error (lost bus arbitration, bus error, ..)
+     */
+    enum at24c_error {
+      ERR_OK = 0,
+      ERR_TOOBIG,
+      ERR_NACK,
+      ERR_DNACK,
+      ERR_BUS
+    };
+
+    AT24Cxxx(uint8_t address, TwoWire& theWire, int writeDelay, uint16_t size, uint8_t pageSize);
+    void setWire(TwoWire& theWire);
+    void setWire(TwoWire *theWire);
+
+    uint8_t read(int idx);
+    void write(int idx, uint8_t val);
+    void update(int idx, uint8_t val);
+    uint16_t length(void);
+    int writeBuffer(uint16_t address, const uint8_t *data, size_t len);
+    int readBuffer(uint16_t address, uint8_t *data, size_t len);
 
     /**
      * Returns result from the last performed operation.
-     * The meaning of the values are:
-     *  0 .. success
-     *  1 .. length to long for buffer
-     *  2 .. address send, NACK received - typically means no device at the address
-     *  3 .. data send, NACK received
-     *  4 .. other twi error (lost bus arbitration, bus error, ..)
      */
-    uint8_t getLastError();
+    uint8_t getLastError(void);
 
-    template< typename T > T &get( int idx, T &t ){
+    template< typename T > T &get(int idx, T &t) {
       readBuffer(idx, (uint8_t*)&t, sizeof(T));
       return t;
     }
 
-    template< typename T > const T &put( int idx, const T &t ){
+    template< typename T > const T &put(int idx, const T &t) {
       writeBuffer(idx, (uint8_t*)&t, sizeof(T));
       return t;
     }
 
   protected:
     virtual void writeAddress(uint16_t address);
-    uint8_t i2cAddress;
-    TwoWire* twoWire;
+    uint8_t _i2cAddress;
+    TwoWire *_wire;
 
   private:
     int rawWriteBuffer(uint16_t address, const uint8_t* data, size_t len);
-    int size;
-    uint8_t writeDelay;
-    uint8_t lastError;
-    uint8_t pageSize;
+    int _size;
+    uint8_t _writeDelay;
+    uint8_t _lastError;
+    uint8_t _pageSize;
 };
 
 
